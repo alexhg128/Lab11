894d85bffd6491b002151d70aca9c297f59173a7
Alejandro Hahn <halexhg@gmail.com>
2020-05-01 13:00:41 -0600

    Lab 11 Finished

---
+++ src/main/java/mx/tec/lab/testbdd/TestbddApplication.java
@@ -0,0 +1,13 @@
+package mx.tec.lab.testbdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestbddApplication.class, args);
+    }
+
+}
